a36ce5a8ebad2b817e7d7aa6a32a67ef3e78552f
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-10 11:22:50 +0200

    5 files modified, 2 added, 2 deleted

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,10 +1,17 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css'],
+  styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent {
 
+  @Input()items = [];
+
+  @Output() selectedItemEmitter = new EventEmitter();
+
+  selectItem(item) {
+    this.selectedItemEmitter.emit(item);
+  }
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css'],
+  styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
+  filterByType = '';
+  filteredItems = [];
+  items = [
+    { name: 'Green', type: 'color' },
+    { name: 'Dog', type: 'animal' },
+    { name: 'Apple', type: 'fruit' },
+    { name: 'Mango', type: 'fruit' },
+    { name: 'Yellow', type: 'color' },
+    { name: 'Cat', type: 'animal' },
+    { name: 'Blue', type: 'color' },
+    { name: 'Red', type: 'color' },
+    { name: 'Cow', type: 'animal' },
+    { name: 'Goat', type: 'animal' },
+    { name: 'Black', type: 'color' },
+    { name: 'Banana', type: 'fruit' },
+    { name: 'Cherry', type: 'fruit' },
+  ];
+  selectedItem: any;
+
+  ngOnInit() {
+    this.filteredItems = [...this.items];
+  }
+
+  filterBy(type) {
+    this.filteredItems = [...this.items.filter((x) => x.type === type)];
+  }
+
+  selectItem(item) {
+    this.selectedItem = item;
+  }
   
 }
